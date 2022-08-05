15e6cfd78956c927ebf5502f81f2f78407a0d94b
johannes karoff <johannes.karoff@priceloop.ai>
2022-08-05 15:28:45 +0200

    install boto3 in ami

--- imagebuild.sh
+++ imagebuild.sh
@@ -4,6 +4,7 @@ set -e
 
 amazon-linux-extras install postgresql14
 yum -y install postgresql-server postgresql-server-devel postgresql-plpython3 git gcc jq
+pip3 install boto3
 
 make -C /postgres_extension/functions
 make -C /postgres_extension/functions install
