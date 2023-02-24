4662d96065c0cfbc2b32eb3bbe51921e2dc8e4de
johannes karoff <johannes.karoff@priceloop.ai>
2023-02-24 10:17:56 +0100

    Update imagebuild.sh

--- imagebuild.sh
+++ imagebuild.sh
@@ -4,20 +4,10 @@ set -Eeuo pipefail
 
 amazon-linux-extras install postgresql14
 yum -y install postgresql-server postgresql-server-devel postgresql-plpython3 git gcc jq
-pip3 install boto3 requests
+pip3 install requests
 
 make -C /postgres_extension/functions
 make -C /postgres_extension/functions install
-
-mkdir -p /postgres_extension/postgres-aws-s3
-cd /postgres_extension/postgres-aws-s3
-git init
-git remote add origin https://github.com/chimpler/postgres-aws-s3
-git fetch --depth 1 origin 4f601abf6960e929db672b46161fcc4313b80959
-git checkout FETCH_HEAD
-
-make -C /postgres_extension/postgres-aws-s3
-make -C /postgres_extension/postgres-aws-s3 install
 
 # pgbackrest
 yum -y install libxml2-devel libyaml-devel lz4-devel libzstd-devel bzip2-devel
