d4ecfe32c2b88ba0190513d2887fb46497043183
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-08-30 17:00:43 +0200

    add plv8 (#7)
    Co-authored-by: Felix Rath <felix.rath@priceloop.ai>
    Co-authored-by: Arne Döring <arne.doering@priceloop.ai>
    Co-authored-by: Felix Dietze <felix.dietze@priceloop.ai>

--- imagebuild.sh
+++ imagebuild.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
 
-set -Eeuo pipefail
+set -Eeuxo pipefail
 
 amazon-linux-extras install postgresql14
-yum -y install postgresql-server postgresql-server-devel postgresql-plpython3 git gcc jq
+yum -y install postgresql-server postgresql-server-devel postgresql-plpython3 git gcc jq wget tar make xz \
+               libxml2-devel libyaml-devel lz4-devel libzstd-devel bzip2-devel \
+               ncurses-compat-libs
+
+
 pip3 install requests
 
-make -C /postgres_extension/functions
-make -C /postgres_extension/functions install
+cd /
+wget https://github.com/Kitware/CMake/releases/download/v3.27.4/cmake-3.27.4-linux-x86_64.tar.gz
+tar -xvzf cmake-3.27.4-linux-x86_64.tar.gz
+export PATH="/cmake-3.27.4-linux-x86_64/bin:${PATH}"
+
+CPU_COUNT=$(nproc --all)
+
+PLV8_BRANCH=v3.2.0
+mkdir -p /build/
+cd /build
+git clone --branch "$PLV8_BRANCH" --depth 1 https://github.com/plv8/plv8
+
+sed -i 's/cmake -Denable/CXX=clang++ CC=clang cmake -Denable/' plv8/Makefile
+make -C plv8 -j"$CPU_COUNT" plv8_config.h
+make -C plv8 -j"$CPU_COUNT" install
+make -C /postgres_extension/functions -j"$CPU_COUNT"
+make -C /postgres_extension/functions -j"$CPU_COUNT" install
 
 # pgbackrest
-yum -y install libxml2-devel libyaml-devel lz4-devel libzstd-devel bzip2-devel
 mkdir -p /pgbackrest-build
 wget -q -O - https://github.com/pgbackrest/pgbackrest/archive/release/2.42.tar.gz | tar zx -C /pgbackrest-build
 cd /pgbackrest-build/pgbackrest-release-2.42/src && ./configure && make
