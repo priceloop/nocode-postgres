c92dbec96d864844c9f187a2a655783c804109d8
Arne Döring <arne.doering@priceloop.ai>
2023-08-30 16:12:49 +0200

    wip

--- imagebuild.sh
+++ imagebuild.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-set -Eeuox pipefail
+set -Eeuxo pipefail
 
 amazon-linux-extras install postgresql14
 yum -y install postgresql-server postgresql-server-devel postgresql-plpython3 git gcc jq wget tar make xz \
                libxml2-devel libyaml-devel lz4-devel libzstd-devel bzip2-devel \
                ncurses-compat-libs
 
+
 pip3 install requests
+
+cd /
+wget https://github.com/Kitware/CMake/releases/download/v3.27.4/cmake-3.27.4-linux-x86_64.tar.gz
+tar -xvzf cmake-3.27.4-linux-x86_64.tar.gz
+export PATH="/cmake-3.27.4-linux-x86_64/bin:${PATH}"
 
 CPU_COUNT=$(nproc --all)
 
-PLV8_BRANCH=v2.3.15
+PLV8_BRANCH=v3.2.0
+mkdir -p /build/
+cd /build
 git clone --branch "$PLV8_BRANCH" --depth 1 https://github.com/plv8/plv8
 
-# sed -i 's/cmake -Denable/CXX=clang++ CC=clang cmake -Denable/' plv8/Makefile
+sed -i 's/cmake -Denable/CXX=clang++ CC=clang cmake -Denable/' plv8/Makefile
 make -C plv8 -j"$CPU_COUNT" plv8_config.h
 make -C plv8 -j"$CPU_COUNT" install
 make -C /postgres_extension/functions -j"$CPU_COUNT"
